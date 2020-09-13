bf8252e1e68c8d0e507a1beb9c09cb6657c22b16
Mayur Panchal <mayur.panchal@vuclip.com>
2020-09-13 12:49:17 +0530

    added 404, theme icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
+import sun from "../img/sun.svg"
+import moon from "../img/moon.svg"
 import "./Header.scss"
 
 const Header = () => {
+  const [isDark, setDark] = useState(false)
+
   return (
     <header className="header">
       <span className="title">Mayur Panchal</span>
@@ -9,6 +13,12 @@ const Header = () => {
         <a href="#work">Work</a>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
+        <img
+          src={isDark ? sun : moon}
+          alt="theme"
+          onClick={() => setDark(!isDark)}
+          className="theme"
+        />
       </nav>
     </header>
   )
