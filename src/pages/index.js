c2e5887811a642f675bd1cfdcd22a269fe72816a
Mayur Panchal <mayur.panchal@vuclip.com>
2020-09-13 13:02:54 +0530

    fixed title issue and favicon not showing

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,24 +8,7 @@ export default function Home() {
   return (
     <div>
       <Helmet>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="static/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="static/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="static/favicon-16x16.png"
-        />
-        <link rel="manifest" href="static/site.webmanifest" />
+        <title>Mayur Panchal</title>
       </Helmet>
       <Header />
       <Main />
