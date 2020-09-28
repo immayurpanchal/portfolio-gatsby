945034090dcc60cc41e475f184295b81dd2aa562
Mayur Panchal <mayur.panchal@vuclip.com>
2020-09-28 12:40:36 +0530

    Added GQL support for social icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,52 +1,49 @@
 import React from "react"
 import "./Footer.scss"
-
-const socialIcons = [
-  {
-    profileUrl: "https://twitter.com/immayurpanchal",
-    imageUrl:
-      "https://ik.imagekit.io/immayurpanchal/Portfolio/twitter-seeklogo.com_n76COVB9n6.svg",
-    alt: "Twitter",
-  },
-  {
-    profileUrl: "https://facebook.com/immayurpanchal",
-    imageUrl:
-      "https://ik.imagekit.io/immayurpanchal/Portfolio/facebook-2_GorFqYO6yy.svg",
-    alt: "Facebook",
-  },
-  {
-    profileUrl: "https://instagram.com/immayurpanchal",
-    imageUrl:
-      "https://ik.imagekit.io/immayurpanchal/Portfolio/instagram-2016_Yx6Zx-gv_s.svg",
-    alt: "Instagram",
-  },
-  {
-    profileUrl: "https://github.com/immayurpanchal",
-    imageUrl:
-      "https://ik.imagekit.io/immayurpanchal/Portfolio/github-1_ma2qvGuCN.svg",
-    alt: "Github",
-  },
-]
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const {
+    allPortfolioJson: { edges },
+  } = useStaticQuery(graphql`
+    {
+      allPortfolioJson {
+        edges {
+          node {
+            id
+            socialLinks {
+              profileUrl
+              imageUrl
+              alt
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const socialIcons = edges.filter(edge => edge.node.socialLinks)[0].node
+    .socialLinks
+
   return (
     <footer className="footer" id="contact">
       <hr />
       <div className="footer-detail">
-        <span className="copyright">© Copyright 2020 | Mayur Panchal</span>
+        <span className="copyright">{`© Copyright ${new Date().getFullYear()} | Mayur Panchal`}</span>
         <div className="social-icons">
-          {socialIcons.map(icon => (
-            <a
-              href={icon.profileUrl}
-              rel="noreferrer"
-              target="_blank"
-              key={icon.alt}
-            >
-              <div className="icon">
-                <img src={icon.imageUrl} alt={icon.alt} />
-              </div>
-            </a>
-          ))}
+          {socialIcons &&
+            socialIcons.map(icon => (
+              <a
+                href={icon.profileUrl}
+                rel="noreferrer"
+                target="_blank"
+                key={icon.alt}
+              >
+                <div className="icon">
+                  <img src={icon.imageUrl} alt={icon.alt} />
+                </div>
+              </a>
+            ))}
         </div>
       </div>
     </footer>
